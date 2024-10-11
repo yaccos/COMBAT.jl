3710444080fe9305cd335a99680a44270ef0167f
Jakob Peder Pettersen <jakobpeder.pettersen@gmail.com>
2024-10-11 10:41:54 +0200

    Failed making a suitable MWE for the problem at hand

--- tuple_allocation_MWE.jl
+++ tuple_allocation_MWE.jl
@@ -1,43 +1,22 @@
-using StaticArrays
 using BenchmarkTools
 using Unitful
 
-struct MyType{T <: AbstractVector}
-    val::T
-end
+# Base.@constprop :aggressive f(x::AbstractArray,y::AbstractArray) = sum(x) * sum(y)
+
+Base.@constprop :aggressive f(x::AbstractArray,y::AbstractArray) = x[1] * y[1]
+
+Base.@constprop :aggressive f(x::Number,y::Number) = x * y
 
 
-f(x::AbstractArray,y::AbstractArray) = sum(x) * sum(y)
+x_unitful = [1u"m"]
 
-@noinline f_wrapper(args) = f(args...)
+y_unitful =  [3u"m"]
 
-function f_wrapper_1(x::MyType,y::MyType)
-    f(x.val,y.val)
-end
 
-function f_wrapper_2(x::MyType,y::MyType)
-    args = (x.val,y.val)
-    f_wrapper(args)
-end
+x_unitless =  [1]
 
-x_static = MyType(@SVector [1u"m",2u"m"])
+y_unitless = [3]
 
-y_static = MyType(@SVector [3u"m",4u"m"])
 
-x_nonstatic = MyType([1u"m",2u"m"])
-
-y_nonstatic = MyType([3u"m",4u"m"])
-
-@btime f_wrapper_1(x_static,y_static)
-
-@btime f_wrapper_1(x_nonstatic,y_nonstatic)
-
-@btime f_wrapper_2(x_static,y_static)
-
-@btime f_wrapper_2(x_nonstatic,y_nonstatic)
-
-@code_llvm f_wrapper_1(x_nonstatic,y_nonstatic)
-
-@code_llvm f_wrapper_2(x_nonstatic,y_nonstatic)
-
-@code_warntype f_wrapper_1(x_static,y_static) 
+@btime $x_unitful .+ $y_unitful
+@btime $x_unitless .+ $y_unitless
